798d798fbcbc3e5ebcf3aae0323f8f16b3414be7
Saalty <43245524+Saalty@users.noreply.github.com>
2021-01-04 17:17:18 +0000

    really super bad gui dont shout out me ik it's not good

--- src/main/java/net/evergreen/client/gui/mainmenu/GuiEvergreenMainMenu.java
+++ src/main/java/net/evergreen/client/gui/mainmenu/GuiEvergreenMainMenu.java
@@ -8,12 +8,13 @@
 
 package net.evergreen.client.gui.mainmenu;
 
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.ScaledResolution;
+import net.evergreen.client.gui.elements.GuiButtonExt;
+import net.minecraft.client.gui.*;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 
 import java.awt.*;
+import java.io.IOException;
 
 public class GuiEvergreenMainMenu extends GuiScreen {
 
@@ -21,7 +22,10 @@ public class GuiEvergreenMainMenu extends GuiScreen {
 
     @Override
     public void initGui() {
-
+        this.buttonList.add(new GuiButtonExt(0, width / 2 - 1 - 20, height - 20, 20, 20, "S"));
+        this.buttonList.add(new GuiButtonExt(1, width / 2 + 1, height - 20, 20, 20, "M"));
+        this.buttonList.add(new GuiButtonExt(2, width - 20, 0, 20, 20, "X"));
+        this.buttonList.add(new GuiButtonExt(3, 0, 0, 20, 20, "O"));
     }
 
     @Override
@@ -32,9 +36,28 @@ public class GuiEvergreenMainMenu extends GuiScreen {
         GlStateManager.enableAlpha();
         ScaledResolution res = new ScaledResolution(mc);
         mc.getTextureManager().bindTexture(ICON);
-        drawScaledCustomSizeModalRect((res.getScaledWidth() / 2) - (100 / 2), 0, 0, 0, 2000, 2000, 100, 100, 2000, 2000);
+        drawScaledCustomSizeModalRect((res.getScaledWidth() / 2) - (100 / 2), 40, 0, 0, 2000, 2000, 100, 100, 2000, 2000);
         GlStateManager.disableAlpha();
         GlStateManager.popMatrix();
         super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        switch (button.id) {
+            case 0:
+                mc.displayGuiScreen(new GuiSelectWorld(this));
+                break;
+            case 1:
+                mc.displayGuiScreen(new GuiMultiplayer(this));
+                break;
+            case 2:
+                mc.shutdown();
+                break;
+            case 3:
+                mc.displayGuiScreen(new GuiOptions(this, mc.gameSettings));
+                break;
+        }
+        super.actionPerformed(button);
     }
 }
